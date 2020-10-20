0fb3b28009cb5c19b93206eab5d3ed1ac05f3d83
Kaushik Mishra <kmishra27.km@gmail.com>
2020-10-20 17:09:59 +0530

    include file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include_once 'header.php';
+?>
+
+        <section>
+            <h1>A title</h1>
+            <p>Some content</p>
+        <section>
+    
+        <footer>
+            <p>A footer</p>
+        </footer>
+
+    </body>
+</html>
