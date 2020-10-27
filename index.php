fdfd504fb5f60a53e74e0c2bc5672e6252738ded
Kaushik Mishra <kmishra27.km@gmail.com>
2020-10-28 00:21:42 +0530

    day of week

--- index.php
+++ index.php
@@ -1,43 +1,30 @@
 <?php
     include_once 'header.php';
 ?>
-
-        <form>
-            <input type="text" name="num1" placeholder="Number 1">
-            <input type="text" name="num2" placeholder="Number 2">
-            <select name="operator">
-                <option>None</option>
-                <option>Add</option>
-                <option>Subtract</option>
-                <option>Multiply</option>
-                <option>Divide</option>
-            </select>
-            <br>
-            <button type="submit" name="submit" value="submit">Calculate</button>
-        </form>
-        <p>The answer is:</p>
     <?php
-        if(isset($_GET['submit'])) {
-            $result1 = $_GET['num1'];
-            $result2 = $_GET['num2'];
-            $operator = $_GET['operator'];
-            switch($operator) {
-                case "None" :
-                    echo "You need to select a method";
-                break;
-                case "Add" :
-                    echo $result1 + $result2;
-                break;
-                case "Subtract" :
-                    echo $result1 - $result2;
-                break;
-                case "Multiply" :
-                    echo $result1 * $result2;
-                break;
-                case "Divide" :
-                    echo $result1/$result2;
-                break;
-            }
+        $dayofweek = date("w");
+        switch($dayofweek) {
+            case 1:
+                echo "It is Monday";
+            break;
+            case 2:
+                echo "It is Tuesday";
+            break;
+            case 3:
+                echo "It is Wednesday";
+            break;
+            case 4:
+                echo "It is Thursday";
+            break;
+            case 5:
+                echo "It is Friday";
+            break;
+            case 6:
+                echo "It is Saturday";
+            break;
+            case 0:
+                echo "It is Sunday";
+            break;
         }
     ?>
     </body>
